f1b101daaa2c183ed9d1907d9d850c25cc3e8d1b
root <root@localhost.localdomain>
2020-04-20 09:18:00 +0000

    1st deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const exec = require('child_process').exec;
+// const exec = require('child_process').exec;
 // サーバをインスタンス化する 
 const app = express();
 
@@ -16,8 +16,27 @@ app.post('/assets/dummy.js',function (req, res, next) {
 // app.use(function (req, res, next) {
   console.log('postest:', Date.now());
   console.log('postest_:', require('child_process'));
-  exec("C:/WINDOWS/system32/notepad.exe C:/work/test.txt");
-  next() 
+  // exec("C:/WINDOWS/system32/notepad.exe C:/work/test.txt");
+  const { exec } = require('child_process');
+  const Encoding = require('encoding-japanese');
+
+  exec('dir', { encoding: 'Shift_JIS' }, (error, stdout, strerr) => {
+    if (error) {
+      console.error(`[ERROR] ${error}`);
+      return;
+    }
+    const toString = (bytes) => {
+      return Encoding.convert(bytes, {
+        from: 'SJIS',
+        to: 'UNICODE',
+        type: 'string',
+      });
+    };
+    console.log(`stdout: ${toString(stdout)}`);
+    console.log(`strerr: ${toString(strerr)}`);
+  });
+
+  next(); 
 });
 
 // サーバ起動
