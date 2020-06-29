04ab0f0b7731c2675d02411a83629f393b87dbfb
root <root@localhost.localdomain>
2020-06-29 07:04:30 +0000

    v1.2.0 URLroute

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/dist/index.html`));
 });
 
-if(process.env.NODE_ENV === 'production'){
-  //set static folder
-  app.use(express.static('client/build'));
-}  
+// if(process.env.NODE_ENV === 'production'){
+//   //set static folder
+//   app.use(express.static('client/build'));
+// }  
 
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.get('*',(req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 // app.post('/assets/dummy.js',function (req, res, next) {
 // // app.use(function (req, res, next) {
