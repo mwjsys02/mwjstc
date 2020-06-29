9891636db43a0d8c17596bdfedba4cb372308f70
root <root@localhost.localdomain>
2020-06-29 05:04:40 +0000

    v1.1.4 URLrouteparam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,6 @@ import { ViewStockComponent } from  './view-stock/view-stock.component';
 
 
 const routes: Routes = [
-    { path: '', 
-    redirectTo: '/stock',
-    pathMatch: 'full' 
-  },
   {
     path: 'stock',
     component: ViewStockComponent
@@ -20,6 +16,12 @@ const routes: Routes = [
     path: 'stock/:gcd/:scd',
     component: ViewStockComponent
   },
+  { 
+    path: '', 
+    redirectTo: '/stock',
+    pathMatch: 'full' 
+  },
+
   {
     path: '**',
     component: ViewStockComponent
